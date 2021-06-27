c4b1fe362dcee3deeae75873df92b263b51f2012
Jonathan Dyallo <emeraldusk@gmail.com>
2021-06-27 20:43:41 -0300

    target blank

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function traducirESP() {
   $changeNombre.innerText = 'Nombre';
   $changeEmail.innerText = 'Email';
   $changeMensaje.innerText = 'Mensaje';
-  $changeFooter.innerHTML = `<p id="footer-text">&copy; Vectores bajados de <a href="https://worldvectorlogo.com/" class="text-primary">worldvectorlogo</a></p>`;
+  $changeFooter.innerHTML = `<p id="footer-text">&copy; Vectores sacados de <a target="_blank" href="https://worldvectorlogo.com/" class="text-primary">worldvectorlogo</a></p>`;
   $changeSubir.value = 'Enviar';
   $changeText.innerText = 'Soy Jonathan me considero una persona autodidacta; actualmente empecé a tomarme el tiempo de especializarme en varios ámbitos de la programación. Como corto plazo tengo interés en el desarrollo web front end y como largo plazo me interesaría aprender machine learning e inteligencia artificial, también tengo amplio conocimiento en software y hardware para hacer de soporte técnico o atención al cliente en tiendas relacionadas. Aprender es algo que adoro y lo hago en varios ámbitos; espero con ansias poder adquirir un lugar donde brindar, toda mi experiencia y deseos por progresar.';
 }
@@ -31,7 +31,7 @@ function traducirENG() {
   $changeNombre.innerText = 'Name';
   $changeEmail.innerText = 'Email';
   $changeMensaje.innerText = 'Message';
-  $changeFooter.innerHTML = `<p id="footer-text">&copy; SVG files from <a href="https://worldvectorlogo.com/" class="text-primary">worldvectorlogo</a></p>`;
+  $changeFooter.innerHTML = `<p id="footer-text">&copy; SVG files from <a target="_blank" href="https://worldvectorlogo.com/" class="text-primary">worldvectorlogo</a></p>`;
   $changeSubir.value = 'Submit';
   $changeText.innerText = "I'm Jonathan. I consider myself a self-taught person; I am currently starting to take the time to specialize in various areas of programming. As a short term I have an interest in front end web development and as a long term I would be interested in learning machine learning and artificial intelligence, I also have extensive knowledge in software and hardware to provide technical support or customer service in related stores. Learning is something that I adore and I do it in various fields; I look forward to acquiring a place to give, all my experience and wishes to progress.";
 }
