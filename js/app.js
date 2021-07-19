3958e828e698eb6337ae706b496efcaaec3d91df
Jonathan Dyallo <emeraldusk@gmail.com>
2021-07-19 19:31:06 -0300

    no funciona

--- js/app.js
+++ js/app.js
@@ -36,24 +36,24 @@ function traducirENG() {
   $changeText.innerText = "I'm Jonathan. I consider myself a self-taught person; I am currently starting to take the time to specialize in various areas of programming. As a short term I have an interest in front end web development and as a long term I would be interested in learning machine learning and artificial intelligence, I also have extensive knowledge in software and hardware to provide technical support or customer service in related stores. Learning is something that I adore and I do it in various fields; I look forward to acquiring a place to give, all my experience and wishes to progress.";
 }
 
-let $changeToEsp = document.getElementById('ESP');
-let $changeToEnd = document.getElementById('ENG');
+const $changeToEsp = document.getElementById('ESP');
+const $changeToEnd = document.getElementById('ENG');
 
-let $changeText = document.getElementById('presentacion-es');
-let $changeContact = document.getElementById('contact');
-let $changeSkills = document.getElementById('skills');
-let $changeTitleSkills = document.getElementById('Skills');
-let $changeWorks = document.getElementById('works');
-let $changeTitleWorks = document.getElementById('Works');
-let $changeHome = document.getElementById('home');
-let $changeCV = document.getElementById('CV');
-let $changeTogether = document.getElementById('together');
-let $changeContactMe = document.getElementById('contactme');
-let $changeNombre = document.getElementById('nombre');
-let $changeEmail = document.getElementById('email');
-let $changeMensaje = document.getElementById('mensaje');
-let $changeSubir = document.getElementById('subir');
-let $changeFooter = document.getElementById('footer-text');
+const $changeText = document.getElementById('presentacion-es');
+const $changeContact = document.getElementById('contact');
+const $changeSkills = document.getElementById('skills');
+const $changeTitleSkills = document.getElementById('Skills');
+const $changeWorks = document.getElementById('works');
+const $changeTitleWorks = document.getElementById('Works');
+const $changeHome = document.getElementById('home');
+const $changeCV = document.getElementById('CV');
+const $changeTogether = document.getElementById('together');
+const $changeContactMe = document.getElementById('contactme');
+const $changeNombre = document.getElementById('nombre');
+const $changeEmail = document.getElementById('email');
+const $changeMensaje = document.getElementById('mensaje');
+const $changeSubir = document.getElementById('subir');
+const $changeFooter = document.getElementById('footer-text');
 
 $changeToEsp.addEventListener("click", traducirESP);
 $changeToEnd.addEventListener("click", traducirENG);
